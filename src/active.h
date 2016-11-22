a0076d49a6f1635eb5fda6c0cbe1efe75275e163
vram.vardanian <vram.vardanian@outlook.com>
2016-11-22 05:18:19 +0300

    improve busy waiting loop

--- src/active.h
+++ src/active.h
@@ -3,6 +3,7 @@
 #include <boost/lockfree/queue.hpp>
 #include <atomic>
 #include <thread>
+#include <chrono>
 
 class Active {
     std::atomic_bool m_stopped;
@@ -10,25 +11,34 @@ class Active {
     std::thread m_thr;
 
     static void ThreadFunc(void* this_p) {
+        unsigned empty_spin_count = 0;
+        const unsigned max_empty_spin_count = 200;
+
         Active* this_ptr = static_cast<Active*>(this_p);
         while(!this_ptr->m_stopped) {
             void* p = nullptr;
             if(this_ptr->m_queue.pop(p)) {
+                empty_spin_count = 0;
                 if(p) {
                     this_ptr->HandleItem(p);
                 } else {
                     this_ptr->m_stopped = true;
                 }
-            } 
-
+            } else {
+                // queue has been empty for long enough, stop heating cpu
+                ++empty_spin_count;
+                if(empty_spin_count > max_empty_spin_count) {
+                    std::this_thread::sleep_for(std::chrono::milliseconds(10));
+                }
+            }
         }
     }
 
 protected:
 
     void PushItem(void* item) {
-         while (!m_queue.push(item))
-             ;
+        while (!m_queue.push(item))
+            ;
     }
 
     virtual void HandleItem(void* item) = 0;
